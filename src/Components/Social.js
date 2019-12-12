91c700c0fae0a9a9608a7643a37e89fe60a1d174
Patrick Heflin <patrickslx@yahoo.com>
2019-12-12 06:49:45 -0600

    add links to social media icons

--- src/Components/Social.js
+++ src/Components/Social.js
@@ -14,10 +14,18 @@ function Social() {
           <h1>Willy-Burger #2 - Katy (832) 437-4566</h1>
         </div>
         <div className='social-icons'>
-          <img src={facebook} alt="facebook" />
-          <img src={tripadvisor} alt="tripadvisor" />
-          <img src={foursquare} alt="foursquare" />
-          <img src={yelp} alt="yelp" />
+          <a href="https://www.facebook.com/pages/Willy-Burger/247444148601519" target="_blank">
+            <img src={facebook} alt="facebook" />
+          </a>
+          <a href="http://www.tripadvisor.com/Restaurant_Review-g60737-d2279092-Reviews-Willy_Burger-Beaumont_Texas.html" target="_blank">
+            <img src={tripadvisor} alt="tripadvisor" />
+          </a>
+          <a href="http://www.yelp.com/biz/willy-burger-beaumont" target="_blank">
+            <img src={foursquare} alt="foursquare" />
+          </a>
+          <a href="https://foursquare.com/v/willy-burger/4e2c8301c65bcaf4003a29ee" target="_blank">
+            <img src={yelp} alt="yelp" />
+          </a>
         </div>
       </div>
     </div>

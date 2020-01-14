2becedcf25437923a221f728571521ff038e64a8
Patrick Heflin <patrickslx@yahoo.com>
2020-01-14 07:57:44 -0600

    add responsivess to menu

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -2,15 +2,23 @@ import React from 'react'
 
 import willymenu1 from '../willymenu1.png'
 import willymenu2 from '../willymenu2.png'
+import willymenu10 from '../willymenu1 copy-0-0.png'
+import willymenu11 from '../willymenu1 copy-1-0.png'
+import willymenu12 from '../willymenu1 copy-2-0.png'
+import willymenu20 from '../willymenu2 copy-0-0.png'
+import willymenu21 from '../willymenu2 copy-1-0.png'
+import willymenu22 from '../willymenu2 copy-2-0.png'
 
 function Menu() {
   return (
     <div className='menu-container'>
-      <div className='menu-menu1'>
-        <img src={willymenu1} alt="willy burger menu first page" />
-      </div>
-      <div className='menu-menu2'>
-        <img src={willymenu2} alt="willy burger menu second page" />
+      <div className='menu-menu'>
+        <img className='menu-item' src={willymenu10} alt="willy burger menu first page" />
+        <img className='menu-item' src={willymenu11} alt="willy burger menu first page" />
+        <img className='menu-item' src={willymenu12} alt="willy burger menu first page" />
+        <img className='menu-item' src={willymenu20} alt="willy burger menu second page" />
+        <img className='menu-item' src={willymenu21} alt="willy burger menu second page" />
+        <img className='menu-item' src={willymenu22} alt="willy burger menu second page" />
       </div>
     </div>
   )

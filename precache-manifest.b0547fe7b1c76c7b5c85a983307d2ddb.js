e6ddbd7fbecc739507dfbd987984d2755fe93cbf
Patrick Heflin <patrickslx@yahoo.com>
2021-11-04 10:58:54 -0500

    Updates

---
+++ precache-manifest.b0547fe7b1c76c7b5c85a983307d2ddb.js
@@ -0,0 +1,202 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f5763b1d4082e94d39fe4d1e8ea0688",
+    "url": "/willy-burger/index.html"
+  },
+  {
+    "revision": "4695b8508c5c82e85c0f",
+    "url": "/willy-burger/static/css/2.c1fb602b.chunk.css"
+  },
+  {
+    "revision": "d25d808c22fe3468cd6c",
+    "url": "/willy-burger/static/css/main.20cd17f2.chunk.css"
+  },
+  {
+    "revision": "4695b8508c5c82e85c0f",
+    "url": "/willy-burger/static/js/2.643547a9.chunk.js"
+  },
+  {
+    "revision": "dab28bac8be664d03c5f0a10cfa299e5",
+    "url": "/willy-burger/static/js/2.643547a9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d25d808c22fe3468cd6c",
+    "url": "/willy-burger/static/js/main.b86617f9.chunk.js"
+  },
+  {
+    "revision": "c31e7c659aff799fcf39",
+    "url": "/willy-burger/static/js/runtime-main.b7f3439f.js"
+  },
+  {
+    "revision": "c6bcc166174827118ebcf1e972699660",
+    "url": "/willy-burger/static/media/POW.c6bcc166.png"
+  },
+  {
+    "revision": "38a6fca375ef61e79ac8013d8f7735e3",
+    "url": "/willy-burger/static/media/WB2KatyOpen.38a6fca3.gif"
+  },
+  {
+    "revision": "5145b60bde1dbc5270a22e268ecb73a7",
+    "url": "/willy-burger/static/media/airstream-banner.5145b60b.png"
+  },
+  {
+    "revision": "372c66a2a42e9c31f076398b14b98be6",
+    "url": "/willy-burger/static/media/airstream.372c66a2.png"
+  },
+  {
+    "revision": "734be29e99114439853fbb76681f3f2d",
+    "url": "/willy-burger/static/media/burger_01-thmb.734be29e.png"
+  },
+  {
+    "revision": "b54a625b98cf22ce989166140f4cb81c",
+    "url": "/willy-burger/static/media/burger_01.b54a625b.png"
+  },
+  {
+    "revision": "d4cd6b31e50e76e893e7542a9797df42",
+    "url": "/willy-burger/static/media/burger_02-thmb.d4cd6b31.png"
+  },
+  {
+    "revision": "9df7b5a2cf818b1643ffe5af679ae3b9",
+    "url": "/willy-burger/static/media/burger_02.9df7b5a2.png"
+  },
+  {
+    "revision": "525a3846155297091fa0dfad614af92c",
+    "url": "/willy-burger/static/media/burger_03-thmb.525a3846.png"
+  },
+  {
+    "revision": "f5bc79ac44a8e4d191f1cdac98f193a9",
+    "url": "/willy-burger/static/media/burger_03.f5bc79ac.png"
+  },
+  {
+    "revision": "5b7b238e2810d70446e946f994934159",
+    "url": "/willy-burger/static/media/burger_04-thmb.5b7b238e.png"
+  },
+  {
+    "revision": "87c2e406c650f205e8ab6ab6d54a5ebf",
+    "url": "/willy-burger/static/media/burger_04.87c2e406.png"
+  },
+  {
+    "revision": "ce0a446049791681ac1d0be5bf855348",
+    "url": "/willy-burger/static/media/burger_05-thmb.ce0a4460.png"
+  },
+  {
+    "revision": "cc23e6ca673ab6d6fd02683711a0a6a1",
+    "url": "/willy-burger/static/media/burger_05.cc23e6ca.png"
+  },
+  {
+    "revision": "42249b5dbe73e8a0b9066b38b56b4c6e",
+    "url": "/willy-burger/static/media/burger_06-thmb.42249b5d.png"
+  },
+  {
+    "revision": "de70c30dd4eef3d991820175dc1e9363",
+    "url": "/willy-burger/static/media/burger_06.de70c30d.png"
+  },
+  {
+    "revision": "a1caf0a221bd4b391f871aa3a0863fb8",
+    "url": "/willy-burger/static/media/burger_07-thmb.a1caf0a2.png"
+  },
+  {
+    "revision": "370626622a580070e64f9815ad6b2a16",
+    "url": "/willy-burger/static/media/burger_07.37062662.png"
+  },
+  {
+    "revision": "8cda65cc0a29762309c6565f48d87338",
+    "url": "/willy-burger/static/media/burger_08-thmb.8cda65cc.png"
+  },
+  {
+    "revision": "ffe04f5816ec8971e14dbf7e696f5571",
+    "url": "/willy-burger/static/media/burger_08.ffe04f58.png"
+  },
+  {
+    "revision": "391dc66359eb39e537924ac4ef4a752d",
+    "url": "/willy-burger/static/media/burger_09-thmb.391dc663.png"
+  },
+  {
+    "revision": "87796b0f9e3cf83d46c27779fc0cf5c6",
+    "url": "/willy-burger/static/media/burger_09.87796b0f.png"
+  },
+  {
+    "revision": "7119834678262a5f6f7c5f22ddd307b4",
+    "url": "/willy-burger/static/media/burger_10-thmb.71198346.png"
+  },
+  {
+    "revision": "2bd4884c0e820066da93cf9ada89180e",
+    "url": "/willy-burger/static/media/burger_10.2bd4884c.png"
+  },
+  {
+    "revision": "21f06dead9bc85534dc8354b4e41911d",
+    "url": "/willy-burger/static/media/burger_11-thmb.21f06dea.png"
+  },
+  {
+    "revision": "4b9aafff0ff953986a28805baa533649",
+    "url": "/willy-burger/static/media/burger_11.4b9aafff.png"
+  },
+  {
+    "revision": "f49064c15ca7d07a891e58d192e4e57d",
+    "url": "/willy-burger/static/media/burger_12-thmb.f49064c1.png"
+  },
+  {
+    "revision": "ae0134965a6dda307e808a1b113d39bc",
+    "url": "/willy-burger/static/media/burger_12.ae013496.png"
+  },
+  {
+    "revision": "36b606de6a7b828301b7c75fc28f9b8e",
+    "url": "/willy-burger/static/media/contact.36b606de.png"
+  },
+  {
+    "revision": "f7cc936b2d1c0c3a4355ed9583618ca7",
+    "url": "/willy-burger/static/media/facebook_logo.f7cc936b.png"
+  },
+  {
+    "revision": "ad16e6b310f098d2b3b29a721bb798fe",
+    "url": "/willy-burger/static/media/find_us_min.ad16e6b3.png"
+  },
+  {
+    "revision": "05b69aa2dd61eec517ec9ae7fcce4db1",
+    "url": "/willy-burger/static/media/header_background.05b69aa2.png"
+  },
+  {
+    "revision": "518f9a7acaab5963d7aa76d9bd285c44",
+    "url": "/willy-burger/static/media/tripadvisorexcellence.518f9a7a.png"
+  },
+  {
+    "revision": "aa99dde4486686ea5dd503ce00b204f2",
+    "url": "/willy-burger/static/media/willyburgerburger.aa99dde4.png"
+  },
+  {
+    "revision": "b346f6cb2efc9472e4d1fd2e7b964d2d",
+    "url": "/willy-burger/static/media/willyburgerlogo.b346f6cb.png"
+  },
+  {
+    "revision": "c404618ab82cd312d1f18d1e659b1d9d",
+    "url": "/willy-burger/static/media/willymenu1 copy-0-0.c404618a.png"
+  },
+  {
+    "revision": "73d2232e81128b438a461cb10c58210a",
+    "url": "/willy-burger/static/media/willymenu1 copy-1-0.73d2232e.png"
+  },
+  {
+    "revision": "704cbbeced8d4486cf5fa75db039f91b",
+    "url": "/willy-burger/static/media/willymenu1 copy-2-0.704cbbec.png"
+  },
+  {
+    "revision": "fdae9403e755aa2a54a3a95b36d4a2df",
+    "url": "/willy-burger/static/media/willymenu1.fdae9403.png"
+  },
+  {
+    "revision": "37bab91e4f06b8f9d035820460837999",
+    "url": "/willy-burger/static/media/willymenu2 copy-0-0.37bab91e.png"
+  },
+  {
+    "revision": "c4ffc9411f12975615154e2106350434",
+    "url": "/willy-burger/static/media/willymenu2 copy-1-0.c4ffc941.png"
+  },
+  {
+    "revision": "9a9f3159ddb9139c377c606bf009d925",
+    "url": "/willy-burger/static/media/willymenu2 copy-2-0.9a9f3159.png"
+  },
+  {
+    "revision": "5872cbce0be7ba9b82cc10239a17f210",
+    "url": "/willy-burger/static/media/willymenu2.5872cbce.png"
+  }
+]);
